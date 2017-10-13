b2d2cf8cbbf4bb943de6edd3054be78a9d08b079
dnguyen22 <don.nguyen22@gmail.com>
2017-10-13 16:32:04 -0500

    Test slicing data

--- pandas_practice.py
+++ pandas_practice.py
@@ -3,7 +3,15 @@ import pandas as pd
 
 def test_read_csv():
     df = pd.read_csv('XOM.csv')
-    print(df.head())
+    print(df.head())  # Prints first 5 rows of data
+    print(df.tail())  # Prints last 5 rows of data
+
+
+def slice_data():
+    df = pd.read_csv('XOM.csv')
+    print(df[10:16])  # Prints rows index 10, 11, 12, 13, 14 and 15
+
 
 if __name__ == "__main__":
-    test_read_csv()
+    # test_read_csv()
+    slice_data()
