a8282484175076265808d4614a8cd9d92e6b6542
dnguyen22 <don.nguyen22@gmail.com>
2017-10-13 16:37:18 -0500

    Add get_max_close

--- pandas_practice.py
+++ pandas_practice.py
@@ -5,6 +5,7 @@ def test_read_csv():
     df = pd.read_csv('XOM.csv')
     print(df.head())  # Prints first 5 rows of data
     print(df.tail())  # Prints last 5 rows of data
+    return df
 
 
 def slice_data():
@@ -12,6 +13,15 @@ def slice_data():
     print(df[10:16])  # Prints rows index 10, 11, 12, 13, 14 and 15
 
 
+def get_max_close(df):
+    """Return the maximum closing value.
+
+    """
+    return df['Close'].max()
+
+
 if __name__ == "__main__":
-    # test_read_csv()
+    data_frame = test_read_csv()
     slice_data()
+    print('Max close: ')
+    print(get_max_close(data_frame))
