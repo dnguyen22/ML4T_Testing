9529bdcd14e5ce3e343cf511f437b86f5fd246d7
dnguyen22 <don.nguyen22@gmail.com>
2017-10-13 16:46:39 -0500

    Add plotting

--- pandas_practice.py
+++ pandas_practice.py
@@ -1,10 +1,13 @@
-import pandas as pd
+import pandas as pd  # Used for dataframes
+import matplotlib.pyplot as plt  # Used for plotting
 
 
 def test_read_csv():
     df = pd.read_csv('XOM.csv')
+    """
     print(df.head())  # Prints first 5 rows of data
     print(df.tail())  # Prints last 5 rows of data
+    """
     return df
 
 
@@ -20,8 +23,28 @@ def get_max_close(df):
     return df['Close'].max()
 
 
+def get_mean_volume(df):
+    """Return the mean volume value.
+
+    """
+    return df['Volume'].mean()
+
+
+def plot_adj_close(df):
+    """Plots the adj close value.
+
+    """
+    df['Adj Close'].plot()
+    plt.show()  # Must be called to show plot
+
+
 if __name__ == "__main__":
     data_frame = test_read_csv()
+    """
     slice_data()
     print('Max close: ')
     print(get_max_close(data_frame))
+    print('Mean volume: ')
+    print(get_mean_volume(data_frame))
+    """
+    plot_adj_close(data_frame)
