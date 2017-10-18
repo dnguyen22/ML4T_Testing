bedec00382c16b3ec8b2118f76f9a8720fb0d9fd
dnguyen22 <don.nguyen22@gmail.com>
2017-10-18 15:42:41 -0500

    First commit

---
+++ histograms_and_scatter_plots.py
@@ -0,0 +1,20 @@
+"""Plot a histogram."""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from util import get_data, plot_data, compute_daily_returns
+
+
+if __name__ == "__main__":
+    # Define date range
+    start_date = '2017-09-15'
+    end_date = '2017-10-05'
+    # Read data
+    dates = pd.date_range(start_date, end_date)
+    symbols = ['SPY']
+    df = get_data(symbols, dates)
+    plot_data(df)
+
+    # Compute daily returns
+    daily_returns = compute_daily_returns(df)
+    plot_data(daily_returns, title="Daily returns", ylabel="Daily returns")
