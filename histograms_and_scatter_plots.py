0a988c7f56b2a8859c3154904133bbeb3d1f6499
dnguyen22 <don.nguyen22@gmail.com>
2017-10-18 15:57:20 -0500

    Add histogram

--- histograms_and_scatter_plots.py
+++ histograms_and_scatter_plots.py
@@ -7,7 +7,7 @@ from util import get_data, plot_data, compute_daily_returns
 
 if __name__ == "__main__":
     # Define date range
-    start_date = '2017-09-15'
+    start_date = '2015-09-15'
     end_date = '2017-10-05'
     # Read data
     dates = pd.date_range(start_date, end_date)
@@ -18,3 +18,7 @@ if __name__ == "__main__":
     # Compute daily returns
     daily_returns = compute_daily_returns(df)
     plot_data(daily_returns, title="Daily returns", ylabel="Daily returns")
+
+    # Plot a histogram
+    daily_returns.hist(bins=5)
+    plt.show()
