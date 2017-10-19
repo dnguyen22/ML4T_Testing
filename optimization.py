946947652e840317d303cec8c1c9623afc67c0bc
dnguyen22 <don.nguyen22@gmail.com>
2017-10-19 18:21:43 -0500

    Plot minimum on graph

--- optimization.py
+++ optimization.py
@@ -23,5 +23,6 @@ if __name__ == "__main__":
     Xplot = np.linspace(0.5, 2.5, 21)
     Yplot = f(Xplot)
     plt.plot(Xplot, Yplot)
+    plt.plot(min_result.x, min_result.fun, 'ro')
     plt.title("Minima of an objective function")
     plt.show()
